73b8abf344df9561fe9a0138bc9ca3a0fe9d987c
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2018-09-05 21:13:11 +0100

    gamma correction

--- ring_client/ring_client.py
+++ ring_client/ring_client.py
@@ -24,6 +24,26 @@ class PixelOutOfRangeError(IndexError):
 
 
 class Pixel():
+    # taken from https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+    _gamma_table = [
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+        1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+        2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+        5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+        10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+        17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+        25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+        37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+        51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+        69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+        90, 92, 93, 95, 96, 98, 99, 101,102,104,105,107,109,110,112,114,
+        115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+        144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+        177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+        215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255,
+    ]
+
     _percieved_luminance = np.array([
         0.2126, 0.7152, 0.0722,
     ])
@@ -35,14 +55,16 @@ class Pixel():
         return '<{}:{}>'.format(self.__class__.__name__, self.get_rgbw())
 
     def get_rgbw(self):
-        # For now just r, g, b, 0. this should however be based on percieved luminance
+        # For now just r, g, b, 0.
+        # This might have a better solution:
+        # http://www.mirlab.org/conference_papers/International_Conference/ICASSP%202014/papers/p1214-lee.pdf
         return np.append(self._values, [0]) * 255
 
     def get_rgb(self):
         return self._values * 255
 
     def to_bytes(self):
-        return bytes(round(float(c)) for c in self.get_rgbw())
+        return bytes(self._gamma_table[round(float(c))] for c in self.get_rgbw())
 
 
 class RingDetective(object):
