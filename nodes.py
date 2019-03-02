ca5cb73e63b1d48d6dc86de884d6c1311b7973a3
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2019-03-02 20:21:34 +0000

    black formatter

--- nodes.py
+++ nodes.py
@@ -3,7 +3,9 @@ import threading
 
 import numpy as np
 import matplotlib
+
 matplotlib.use("agg")
+
 from airpixel import client as air_client
 from numpy.fft import rfft as fourier_transform, rfftfreq
 from pyPiper import Node, Pipeline
@@ -26,8 +28,8 @@ class ContiniuousVolumeNormalizer:
         else:
             target_threshold = max_sample
             factor = 1 / self._falloff ** (timestamp - self._last_call)
-            self._current_threshold = self._current_threshold * factor + target_threshold * (
-                1 - factor
+            self._current_threshold = (
+                self._current_threshold * factor + target_threshold * (1 - factor)
             )
         self._last_call = timestamp
 
@@ -80,11 +82,9 @@ class AudioGenerator(PlottableNode):
         super().setup(window)
         self._samples = samples
         self._input_device = audio_input
-    
+
     def run(self, data):
-        samples = np.array(
-            self._input_device.get_samples(self._samples)
-        )
+        samples = np.array(self._input_device.get_samples(self._samples))
         self.emit(samples)
 
 
@@ -92,19 +92,16 @@ class FastFourierTransform(PlottableNode):
     def setup(self, samples, sample_delta, window=None):
         super().setup(window)
         self.sample_delta = sample_delta
-        self.fourier_frequencies = rfftfreq(
-            samples,
-            d=sample_delta,
-        )
+        self.fourier_frequencies = rfftfreq(samples, d=sample_delta)
 
     def run(self, data):
-        self.emit(
-            np.absolute(fourier_transform(data) * self.sample_delta)
-        )
+        self.emit(np.absolute(fourier_transform(data) * self.sample_delta))
 
 
 class OctaveSubsampler(PlottableNode):
-    def setup(self, start_octave, samples_per_octave, num_octaves, frequencies, window=None):
+    def setup(
+        self, start_octave, samples_per_octave, num_octaves, frequencies, window=None
+    ):
         super().setup(window)
         self._sample_points = np.exp2(
             (
@@ -117,22 +114,14 @@ class OctaveSubsampler(PlottableNode):
 
     def run(self, data):
         self.emit(
-            np.interp(
-                self._sample_points,
-                self.frequencies,
-                data,
-                left=0,
-                right=0,
-            )
+            np.interp(self._sample_points, self.frequencies, data, left=0, right=0)
         )
 
 
 class AWeighting(PlottableNode):
     def setup(self, frequencies, window=None):
         self.weights = np.interp(
-            frequencies,
-            a_weighting_table.frequencies,
-            a_weighting_table.weights,
+            frequencies, a_weighting_table.frequencies, a_weighting_table.weights
         )
         super().setup(window)
 
@@ -141,31 +130,26 @@ class AWeighting(PlottableNode):
         self._plot.setLogMode(x=True, y=False)
 
     def run(self, data):
-        self.emit(
-            data * self.weights,
-        )
+        self.emit(data * self.weights)
 
 
 class Gaussian(PlottableNode):
     def setup(self, sigma, window=None):
         self._sigma = sigma
         super().setup(window)
-    
+
     def setup_plot(self, window):
         super().setup_plot(window)
         self._plot.setLogMode(x=True, y=False)
 
     def run(self, data):
-        self.emit(
-            ndimage.gaussian_filter(data, sigma=self._sigma),
-        )
+        self.emit(ndimage.gaussian_filter(data, sigma=self._sigma))
 
 
 class Square(PlottableNode):
     def run(self, data):
-        self.emit(
-            data ** 2,
-        )
+        self.emit(data ** 2)
+
 
 class FoldingNode(PlottableNode):
     def setup(self, num_octaves, window=None):
@@ -176,10 +160,16 @@ class FoldingNode(PlottableNode):
         window.nextRow()
         self._plot = window.addPlot(title=self.name)
         self._curves = [
-            self._plot.plot(pen=(i / self._num_octaves * 255, (1 - i / self._num_octaves) * 255, 255))
+            self._plot.plot(
+                pen=(
+                    i / self._num_octaves * 255,
+                    (1 - i / self._num_octaves) * 255,
+                    255,
+                )
+            )
             for i in range(self._num_octaves)
         ]
-    
+
     def plot(self, data):
         for sub_data, curve in zip(data, self._curves):
             curve.setData(sub_data)
@@ -192,15 +182,12 @@ class FoldingNode(PlottableNode):
 
 class SumMatrixVertical(PlottableNode):
     def run(self, data):
-        self.emit(
-            np.add.reduce(data),
-        )
+        self.emit(np.add.reduce(data))
+
 
 class MaxMatrixVertical(PlottableNode):
     def run(self, data):
-        self.emit(
-            np.maximum.reduce(data),
-        )
+        self.emit(np.maximum.reduce(data))
 
 
 class NaturalLogarithm(PlottableNode):
@@ -212,10 +199,9 @@ class Normalizer(PlottableNode):
     def setup(self, min_threshold=0, falloff=1.1, window=None):
         super().setup(window=window)
         self.normalizer = ContiniuousVolumeNormalizer(
-            min_threshold=min_threshold,
-            falloff=falloff,
+            min_threshold=min_threshold, falloff=falloff
         )
-    
+
     def run(self, data):
         self.emit(self.normalizer.normalize(data, time.time()))
 
@@ -244,12 +230,12 @@ class Fade(PlottableNode):
 class Ring(Node):
     def setup(self, port, num_leds, color_rotation_period):
         self._color_rotation_period = color_rotation_period
-        self.client = air_client.AirClient(port, port + 1, num_leds)
+        self.client = air_client.AirClient(port, port + 1, num_leds, )
         self.client.connect()
-    
+
     def _values_to_rgb(self, values, timestamp):
         hue = np.full(
-            values.shape, 
+            values.shape,
             (timestamp % self._color_rotation_period) / self._color_rotation_period,
         )
         saturation = np.clip(values * (-2), -2, -1) + 2
@@ -257,7 +243,7 @@ class Ring(Node):
         hsvs = np.transpose(np.array((hue, saturation, values_color)))
         rgbs = matplotlib.colors.hsv_to_rgb(hsvs)
         return rgbs
-    
+
     def attempt_connect(self):
         try:
             self.client.connect()
@@ -266,7 +252,7 @@ class Ring(Node):
 
     def run(self, data):
         frame = [
-            air_client.Pixel(g, r, b)
+            air_client.Pixel(r, g, b)
             for r, g, b in self._values_to_rgb(data, time.time())
         ]
         self.client.set_frame(frame)
--- run.py
+++ run.py
@@ -21,7 +21,7 @@ def main() -> None:
     if config.VISUALIZE:
         app = QtGui.QApplication([])
         window = graph.GraphicsWindow(title="Audio")
-        window.resize(1800,600)
+        window.resize(1800, 600)
         window.setWindowTitle("Audio")
     else:
         window = None
@@ -30,29 +30,49 @@ def main() -> None:
     audio_input.start()
 
     samples = audio_input.seconds_to_samples(config.WINDOW_SIZE_SEC)
-    fft_node = nodes.FastFourierTransform("fft", samples=samples, sample_delta=audio_input.sample_delta, window=None)
+    fft_node = nodes.FastFourierTransform(
+        "fft", samples=samples, sample_delta=audio_input.sample_delta, window=None
+    )
 
     pipeline = Pipeline(
-        nodes.AudioGenerator("mic", audio_input=audio_input, samples=samples, window=window)
+        nodes.AudioGenerator(
+            "mic", audio_input=audio_input, samples=samples, window=window
+        )
         | fft_node
-        | nodes.AWeighting("a-weighting", frequencies=fft_node.fourier_frequencies, window=None)
+        | nodes.AWeighting(
+            "a-weighting", frequencies=fft_node.fourier_frequencies, window=None
+        )
         | nodes.Gaussian("smoothed", sigma=1, window=None)
-        | nodes.OctaveSubsampler("sampled", start_octave=5, samples_per_octave=config.NUM_LEDS, num_octaves=config.NUM_OCTAVES, frequencies=fft_node.fourier_frequencies, window=None)
+        | nodes.OctaveSubsampler(
+            "sampled",
+            start_octave=5,
+            samples_per_octave=config.NUM_LEDS,
+            num_octaves=config.NUM_OCTAVES,
+            frequencies=fft_node.fourier_frequencies,
+            window=None,
+        )
         | nodes.FoldingNode("folded", num_octaves=config.NUM_OCTAVES, window=None)
         | nodes.SumMatrixVertical("sum", window=None)
         | nodes.Square("square", window=None)
         # | MaxMatrixVertical("max", window=window)
         | nodes.NaturalLogarithm("log", window=None)
-        | nodes.Normalizer("normalized", min_threshold=config.VOLUME_MIN_THRESHOLD, falloff=config.VOLUME_FALLOFF, window=window)
+        | nodes.Normalizer(
+            "normalized",
+            min_threshold=config.VOLUME_MIN_THRESHOLD,
+            falloff=config.VOLUME_FALLOFF,
+            window=window,
+        )
         | nodes.Fade("fade", falloff=config.FADE_FALLOFF, window=window)
-        | nodes.Ring("ring", port=config.PORT, num_leds=config.NUM_LEDS, color_rotation_period=config.COLOR_RATATION_PERIOD)
+        | nodes.Ring(
+            "ring",
+            port=config.PORT,
+            num_leds=config.NUM_LEDS,
+            color_rotation_period=config.COLOR_RATATION_PERIOD,
+        )
     )
 
     if config.VISUALIZE:
-        audio_pipeline = threading.Thread(
-            target=pipeline.run,
-            daemon=True,
-        )
+        audio_pipeline = threading.Thread(target=pipeline.run, daemon=True)
         audio_pipeline.start()
         app.exec_()
     else:
