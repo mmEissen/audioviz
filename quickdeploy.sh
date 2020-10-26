ae0c152c0a9dce5fefcef904340b9a4ed00af250
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2020-10-26 20:41:38 +0100

    clean up some stuff

--- quickdeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pushd "$(dirname $0)"
-rsync -aP --delete --exclude-from='../airpixel/.gitignore' --exclude='../airpixel/.git/*' ../airpixel/ 192.168.4.1:/home/moritz/audio/airpixel
-rsync -aP --delete --exclude-from='.gitignore' --exclude='.git/*' ./ 192.168.4.1:/home/moritz/audio/audioviz
-popd
-ssh 192.168.4.1 "sudo systemctl restart audioviz" 
