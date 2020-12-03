6d328bb6da0b2c5aeb17b7b25bba6d7a2ea6d274
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2020-12-03 21:03:14 +0100

    print calibration

--- audioviz/calibrate.py
+++ audioviz/calibrate.py
@@ -7,7 +7,8 @@ from audioviz import audio_tools, computations, star
 
 
 CALIBRATION_SAMPLES = 100
-CALIBRATION_FACTOR = 2
+
+CALIBRATION_FACTOR = 3
 
 
 def make_computation():
@@ -21,7 +22,7 @@ def make_computation():
 
 def get_value(computation):
     value = computation.volatile_value()
-    time.sleep(2 * star.WINDOW_SIZE_SEC)
+    time.sleep(star.WINDOW_SIZE_SEC)
     return value
 
 
@@ -29,10 +30,15 @@ def get_value(computation):
 def main() -> None:
     comp = make_computation()
 
-    max_value = max(get_value(comp) for _ in range(CALIBRATION_SAMPLES))
+    max_value = 0
+    for i in range(CALIBRATION_SAMPLES):
+        print(f"{i:4}/{CALIBRATION_SAMPLES}", end="\r")
+        max_value = max(get_value(comp), max_value)
 
+    calibration = max_value * CALIBRATION_FACTOR
+    print(f"Done. Calibrated to {calibration}")
     with open(star.CALIBRATION_FILE, "w") as calibration_file:
-        calibration_file.write(str(max_value * CALIBRATION_FACTOR))
+        calibration_file.write(str(calibration))
 
 
 if __name__ == "__main__":
