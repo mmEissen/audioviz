4e4fb299c6d33c62e9d56d8d01b41dbcd9d0a62c
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2020-12-03 21:06:38 +0100

    stop service

--- dev_install.sh
+++ dev_install.sh
@@ -12,11 +12,12 @@ poetry install --no-dev
 
 sudo cp -f ./audioviz.service /etc/systemd/system/audioviz.service
 
+sudo systemctl stop audioviz
 poetry run python -m audioviz.calibrate
 
 sudo systemctl daemon-reload
 sudo systemctl enable audioviz
-sudo systemctl restart audioviz
+sudo systemctl start audioviz
 
 sudo journalctl -f -u audioviz.service
 
